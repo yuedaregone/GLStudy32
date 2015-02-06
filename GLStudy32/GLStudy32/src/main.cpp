6949d69d0c24303284f970bbc223f7afb66dc2c9
yuedaregone <810566249@qq.com>
2015-02-06 18:37:00 +0800

    opengl texture
    draw something with texture

--- GLStudy32/GLStudy32/src/main.cpp
+++ GLStudy32/GLStudy32/src/main.cpp
@@ -12,6 +12,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include "shader.h"
+#include "texture.hpp"
 //#pragma comment(lib,"glew.lib")
 /************************************************************************/
 /* glDraw																*/
@@ -213,6 +214,199 @@ private:
 	GLuint m_vertexPos_modelspaceID1;*/
 };
 
+class GlDrawTexture
+{
+public:
+	GlDrawTexture()
+	{
+		m_vetexId = 0;
+		m_vetexId1 = 0;
+		m_programId = 0;
+	};
+	~GlDrawTexture() {};
+	void shaderInit()
+	{
+		const char* verStr = "\
+			#version 120													\n\
+			attribute vec3 vertexPosition_modelspace;						\n\
+			attribute vec2 vertexUV;										\n\
+			varying vec2 UV;												\n\
+			uniform mat4 MVP;												\n\
+			void main(){													\n\
+				gl_Position = MVP*vec4(vertexPosition_modelspace, 1.0);		\n\
+				UV = vertexUV;												\n\
+			}																\n\
+		";
+
+		const char* fragStr = "\
+			#version 120											\n\
+			varying vec2 UV;										\n\
+			uniform sampler2D myTextureSampler;						\n\
+			void main(){											\n\
+				gl_FragColor = texture2D(myTextureSampler,UV);		\n\
+			}														\n\
+		";		
+		m_programId = loadShaders(verStr, fragStr);
+		m_vertexPos_modelspaceID = glGetAttribLocation(m_programId, "vertexPosition_modelspace");
+		m_matrixId = glGetUniformLocation(m_programId, "MVP");
+		m_colorId = glGetAttribLocation(m_programId, "vertexUV");
+		//add texture
+		m_texture = loadDDS("texture.dds");
+		m_textureID = glGetUniformLocation(m_programId, "myTextureSampler");
+
+		//create MVP
+		projection = glm::perspective(45.0f, 1.0f/1.0f, 0.1f, 100.0f);
+		View = glm::lookAt(
+			glm::vec3(0, 0, 6), // Camera is at (4,3,3), in World Space
+			glm::vec3(0, 0, 0), // and looks at the origin
+			glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+			);
+		Model = glm::mat4(1.0f);		
+	}
+	void drawInit()
+	{
+		glGenBuffers(1, &m_vetexId);
+		glBindBuffer(GL_ARRAY_BUFFER,m_vetexId);
+		static const GLfloat g_vertex_buff_data[] = {	
+			-1.0f, 1.0f, 1.0f,
+			-1.0f, -1.0f, 1.0f,
+			1.0f, -1.0f, 1.0f,
+			1.0f, 1.0f, 1.0f,
+			-1.0f, 1.0f, 1.0f,
+			1.0f, -1.0f, 1.0f,
+			-1.0f, -1.0f, -1.0f, // triangle 1 : begin
+			-1.0f, -1.0f, 1.0f,
+			-1.0f, 1.0f, 1.0f, // triangle 1 : end
+			1.0f, 1.0f, -1.0f, // triangle 2 : begin
+			-1.0f, -1.0f, -1.0f,
+			-1.0f, 1.0f, -1.0f, // triangle 2 : end
+			1.0f, -1.0f, 1.0f,
+			-1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, 1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			-1.0f, -1.0f, -1.0f,
+			-1.0f, -1.0f, -1.0f,
+			-1.0f, 1.0f, 1.0f,
+			-1.0f, 1.0f, -1.0f,
+			1.0f, -1.0f, 1.0f,
+			-1.0f, -1.0f, 1.0f,
+			-1.0f, -1.0f, -1.0f,			
+			1.0f, 1.0f, 1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, 1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, 1.0f, 1.0f,
+			1.0f, -1.0f, 1.0f,
+			1.0f, 1.0f, 1.0f,
+			1.0f, 1.0f, -1.0f,
+			-1.0f, 1.0f, -1.0f,
+			1.0f, 1.0f, 1.0f,
+			-1.0f, 1.0f, -1.0f,
+			-1.0f, 1.0f, 1.0f		
+		};
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buff_data), g_vertex_buff_data, GL_STATIC_DRAW);
+
+		
+		glGenBuffers(1, &m_vetexId1);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vetexId1);
+		GLfloat g_vertex_buff_data1[] = {	
+			0.000000f, 0.000000f,
+			0.000000f, 1.000000f,
+			1.000000f, 1.000000f,
+			1.000000f, 0.000000f,
+			0.000000f, 0.000000f,
+			1.000000f, 1.000000f,
+			0.000059f, 1.0f - 0.000004f,
+			0.000103f, 1.0f - 0.336048f,
+			0.335973f, 1.0f - 0.335903f,
+			1.000023f, 1.0f - 0.000013f,
+			0.667979f, 1.0f - 0.335851f,
+			0.999958f, 1.0f - 0.336064f,
+			0.667979f, 1.0f - 0.335851f,
+			0.336024f, 1.0f - 0.671877f,
+			0.667969f, 1.0f - 0.671889f,
+			1.000023f, 1.0f - 0.000013f,
+			0.668104f, 1.0f - 0.000013f,
+			0.667979f, 1.0f - 0.335851f,
+			0.000059f, 1.0f - 0.000004f,
+			0.335973f, 1.0f - 0.335903f,
+			0.336098f, 1.0f - 0.000071f,
+			0.667979f, 1.0f - 0.335851f,
+			0.335973f, 1.0f - 0.335903f,
+			0.336024f, 1.0f - 0.671877f,			
+			0.668104f, 1.0f - 0.000013f,
+			0.335973f, 1.0f - 0.335903f,
+			0.667979f, 1.0f - 0.335851f,
+			0.335973f, 1.0f - 0.335903f,
+			0.668104f, 1.0f - 0.000013f,
+			0.336098f, 1.0f - 0.000071f,
+			0.000103f, 1.0f - 0.336048f,
+			0.000004f, 1.0f - 0.671870f,
+			0.336024f, 1.0f - 0.671877f,
+			0.000103f, 1.0f - 0.336048f,
+			0.336024f, 1.0f - 0.671877f,
+			0.335973f, 1.0f - 0.335903f					
+		};
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buff_data1), g_vertex_buff_data1, GL_STATIC_DRAW);
+
+		m_MVP = projection*View*Model;
+	}
+	void actionUpdate()
+	{
+		return;
+		Model = glm::rotate(Model, 0.001f, glm::vec3(1, 1, 1));
+		//Model = glm::translate(Model, glm::vec3(0.01f, 0, 0));		
+		m_MVP = projection*View*Model;
+	}
+	void drawUpdate()
+	{		
+		glUseProgram(m_programId);			
+		
+		glUniformMatrix4fv(m_matrixId, 1, GL_FALSE, &m_MVP[0][0]);
+
+		//get texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, m_texture);
+		glUniform1i(m_textureID, 0);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(m_vertexPos_modelspaceID);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_vetexId);
+		glVertexAttribPointer(
+			m_vertexPos_modelspaceID,// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,					// size
+			GL_FLOAT,			// type
+			GL_FALSE,			// normalized?
+			0,					// stride
+			(void*)0);			// array buffer offset
+		//set color property
+		glEnableVertexAttribArray(m_colorId);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vetexId1);
+		glVertexAttribPointer(m_colorId, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 36); // Starting from vertex 1; 3 vertices total -&gt; 1 triangle
+		glDisableVertexAttribArray(m_colorId);
+		glDisableVertexAttribArray(m_vertexPos_modelspaceID);	
+	}
+private:
+	GLuint m_vetexId;
+	GLuint m_vetexId1;
+	GLuint m_programId;
+	GLuint m_vertexPos_modelspaceID;
+	GLuint m_matrixId;
+	GLuint m_colorId;
+	GLuint m_texture;
+	GLuint m_textureID;
+	glm::mat4 projection;
+	glm::mat4 View;
+	glm::mat4 Model;
+	glm::mat4 m_MVP;
+	/*GLuint m_programId1;
+	GLuint m_vertexPos_modelspaceID1;*/
+};
+
 int main(void)
 {
 	if (!glfwInit()){ //init glfw	
@@ -240,7 +434,7 @@ int main(void)
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 	
-	GlDraw glDraw; //gl draw class
+	GlDrawTexture glDraw; //gl draw class
 	glDraw.shaderInit();
 	glDraw.drawInit();
 	
@@ -248,6 +442,7 @@ int main(void)
 	while (isRun) { //gl draw main loop
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	//clear color	
 		
+		glDraw.actionUpdate();
 		glDraw.drawUpdate(); //gl draw update
 		
 		glfwSwapBuffers(window);
